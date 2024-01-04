42b9eca0b9f25578b292c98cb339654f683f7bb1
Prabhu Shanmugasundaram <prabhucs45@gmail.com>
2024-01-04 19:03:27 +0530

    Express API - MERN

---
+++ index.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const mongoose = require('mongoose')
+const PhoneBook = require('./model/PhoneBook')
+
+app.use(express.json())
+app.use(cors())
+
+const PORT = 8000
+
+app.listen(PORT, () => {
+    connectMongo();
+    console.log(`Server is running on PORT ${PORT}...`)
+})
+
+//DB Connection
+const connectMongo = async () => {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/Mern');
+        console.log('DB Connection Success');
+    }
+    catch (error){
+        throw error;
+    }
+}
+
+app.post('/add-phone', async(req,res) => {
+    const phoneNumber = new PhoneBook(req.body)
+
+    try {
+        await phoneNumber.save()
+        res.status(201).json({
+            status: 'Success',
+            data: {
+                phoneNumber
+            }
+        })
+    } catch(err){
+        res.status(500).json({
+            status: 'Failed',
+            message: err
+        })
+    }
+})
+
+app.get('/get-phone', async(req,res) => {
+    const phoneNumbers = await PhoneBook.find({})
+    try {
+        res.status(200).json({
+            status: 'Success',
+            data: {
+                phoneNumbers
+            }
+        })
+    } catch(err){
+        res.status(500).json({
+            status: 'Failed',
+            message: err
+        })
+    }
+})
+
+app.patch('/update-phone/:id', async (req,res) => {
+    const updatedPhone = await PhoneBook.findByIdAndUpdate(req.params.id,req.body,{
+        new : true,
+        runValidators : true
+      })
+    try{
+        res.status(200).json({
+            status : 'Success',
+            data : {
+              updatedPhone
+            }
+          })
+    }catch(err){
+        console.log(err)
+    }
+})
+
+
+app.delete('/delete-phone/:id', async(req,res) => {
+    await PhoneBook.findByIdAndDelete(req.params.id)
+    
+    try{
+      res.status(204).json({
+          status : 'Success',
+          data : {}
+      })
+    }catch(err){
+        res.status(500).json({
+            status: 'Failed',
+            message : err
+        })
+    }
+})
+
